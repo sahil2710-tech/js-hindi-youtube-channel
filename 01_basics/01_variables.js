bff0a4b8588637a2d21710af7752c7fb4d5ac166
sahil2710-tech <230101120172@centurionuniv.edu.in>
2025-12-30 14:43:58 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 445566
+let accountEmail = "sing123@gmail.com"
+var accountPassword = "singh"
+accountCity = "mumbai"
+
+//  accountId = 22
+
+accountEmail = "ss@gmail.com"
+accountPassword = "sahil"
+accontCity = "gaya"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPassword])
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
