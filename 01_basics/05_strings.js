e58815a3a7bcbbac6acabe1957617b0915daef29
sahil2710-tech <230101120172@centurionuniv.edu.in>
2026-01-06 15:45:12 +0000

    nums_and_math

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,6 +3,33 @@ const repoCount = 50
 
 //  console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
 
-const gameName = new String('Raaj')
+const gameName = new String('raj-singh-hc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('r'));
+
+const newString = gameName.substring(2, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-10, 2)
+console.log(anotherString);
+
+const newStringOne ="     Raj     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://raj.com/raj%20singh"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('sahil'))
+
+console.log(gameName.split('-'));
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 12.84955
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//  +++++++++++++++++++++++++++MAths+++++++++++++++++++++++++++++++++
+ 
+// console.log(Math);
+// console.log(Math.abs(-45));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.max(4,5,13,2));
+// console.log(Math.min(4,2,88,4));
+
+console.log(Math.random()); // gives random value between 0 and 1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
